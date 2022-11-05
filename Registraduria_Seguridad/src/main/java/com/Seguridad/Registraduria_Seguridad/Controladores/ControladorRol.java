5097e5ff8ac5be8fe38bf2c42658b75f704f4a96
“mintic03” <narensilva@gmail.com>
2022-11-05 10:30:44 -0500

    PermisoRol

---
+++ Registraduria_Seguridad/src/main/java/com/Seguridad/Registraduria_Seguridad/Controladores/ControladorRol.java
@@ -0,0 +1,52 @@
+package com.Seguridad.Registraduria_Seguridad.Controladores;
+
+import com.Seguridad.Registraduria_Seguridad.Modelos.Rol;
+import com.Seguridad.Registraduria_Seguridad.Repositorios.RepositorioRol;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/Roles")
+public class ControladorRol {
+
+    @Autowired
+    private RepositorioRol miRepositorioRol;
+
+    @GetMapping("")
+    public List<Rol> index(){
+        return this.miRepositorioRol.findAll();
+    }
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Rol create(@RequestBody Rol infoRol){
+        return this.miRepositorioRol.save(infoRol);
+    }
+    @PutMapping("{id}")
+    public Rol update(@PathVariable String id, @RequestBody Rol infoRol){
+        Rol rol=this.miRepositorioRol.findById(id).orElse(null);
+        if(rol != null){
+            rol.setNombre(infoRol.getNombre());
+            rol.setDescripcion(infoRol.getDescripcion());
+            return this.miRepositorioRol.save(rol);
+        }
+        else {
+            return null;
+        }
+    }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id){
+        Rol rol = this.miRepositorioRol.findById(id).orElse(null);
+        if(rol != null){
+            this.miRepositorioRol.delete(rol);
+        }
+    }
+    @GetMapping("{id}")
+    public Rol show(@PathVariable String id){
+        Rol rol = this.miRepositorioRol.findById(id).orElse(null);
+        return rol;
+    }
+}
---
+++ Registraduria_Seguridad/src/main/java/com/Seguridad/Registraduria_Seguridad/Repositorios/RepositorioUsuario.java
@@ -0,0 +1,4 @@
+package com.Seguridad.Registraduria_Seguridad.Repositorios;
+
+public interface RepositorioUsuario {
+}
