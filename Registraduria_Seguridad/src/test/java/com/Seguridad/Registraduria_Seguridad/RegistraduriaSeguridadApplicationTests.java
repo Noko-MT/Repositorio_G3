c0abdb041fc4b1f02257df120b570b6a30b8c719
LUISMORITZ33 <111940679+LUISMORITZ33@users.noreply.github.com>
2022-11-05 09:27:04 -0500

    Administración Permiso

---
+++ Registraduria_Seguridad/src/test/java/com/Seguridad/Registraduria_Seguridad/RegistraduriaSeguridadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Seguridad.Registraduria_Seguridad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistraduriaSeguridadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
