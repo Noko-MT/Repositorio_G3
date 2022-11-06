ee25f8fdc2a816e34d5b0a5666b326f7fd29db1c
unknown <nadira.aziz.corredor@gmail.com>
2022-11-06 09:33:54 -0500

    Rol

---
+++ Registraduria_Seguridad/src/main/java/com/Seguridad/Registraduria_Seguridad/Modelos/Rol.java
@@ -0,0 +1,31 @@
+package com.Seguridad.Registraduria_Seguridad.Modelos;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+public class Rol {
+    @Id
+    private String _id;
+    private String nombre;
+
+
+    public Rol(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
